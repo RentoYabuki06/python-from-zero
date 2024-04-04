64db3268c9b596cf6a942012dcf6497eb91d7dcc
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-04 15:55:11 +0900

    firstadd:zukei

---
+++ chapter06/zukei.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+# ウィンドウとキャンパスを作成
+win = Tk()
+cv = Canvas(win, width=400,height=300)
+cv.pack()
+
+# 円を描画
+cv.create_oval(120,120,240,240,fill="red",outline="green", width=2)
+
+# 多角形を描画
+cv.create_polygon(210,290,390,290, 390,140, fill="blue",outline="red",width=2)
+
+
+# 長方形を描画
+cv.create_rectangle(10,10,130,110,fill="green", outline="red", width=2)
+
+# メインループを実行
+win.mainloop()
